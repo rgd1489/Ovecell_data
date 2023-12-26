73bba17bbcbe5fec70a61dc2238c2dd230c404a3
RakeshGoud <112722271+rgd1489@users.noreply.github.com>
2023-12-26 20:26:51 +0200

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-# src/app.py
+# app/main.py
 import argparse
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
 import os
 import json
 import csv
-from datetime import datetime
-
-class Database:
-    def __init__(self):
-        # Set up a PostgreSQL database connection
-        self.engine = create_engine('postgresql://ovicell_user:ovicell_password@db:5432/ovicell_db')
-        self.Session = sessionmaker(bind=self.engine)
-
-    def execute(self, query, params=None):
-        # Execute SQL queries on the database
-        with self.engine.connect() as connection:
-            result = connection.execute(query, params)
-        return result
-
-    def execute_script(self, script_path):
-        # Execute SQL script files
-        with open(script_path, 'r') as script_file:
-            query = text(script_file.read())
-            self.execute(query)
+from database import Database
 
 def read_json_file(file_path):
     with open(file_path, 'r') as file:
@@ -77,7 +57,7 @@ def main():
     db = Database()
 
     # Create database schema
-    db.execute_script('app/create_schema.sql')
+    db.execute_script('create_schema.sql')
 
     # Import user accounts
     data_directory = 'data'
